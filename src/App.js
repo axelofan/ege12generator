b0494337f16cd84a17dd43b38e22e184df6cc712
axelofan <cabanov.alexey@yandex.ru>
2020-07-31 14:50:42 +0400

    Create generator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function randint(min, max) {
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
 }
 
-export default App;
+function konjunction(n1, n2) {
+  let result = 0
+  for (let i=0; i<8; i++) {
+    result+= n1%2 * n2%2 * Math.pow(2,i)
+    n1=n1/2>>0
+    n2=n2/2>>0
+  }
+  return result
+}
+
+function generateIP() {
+  const result=[]
+  for(let i=0;i<4;i++) result.push(randint(50,255))
+  return result
+}
+
+function generateMask() {
+  const result=[]
+  const byte = [0, 128, 192, 224, 240, 248, 252, 254, 255]
+  let count1 = randint(16,29)
+  for(let i=0;i<4;i++) {
+    if (count1>8) {
+      result.push(255)
+      count1-=8
+    }
+    else {
+      result.push(byte[count1])
+      count1=0
+    }
+  }
+  return result
+}
+
+function getNet(ip, mask) {
+  let result=[]
+  for(let i=0;i<4;i++) result.push(konjunction(ip[i],mask[i]))
+  return result
+}
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {ip: generateIP(), mask: generateMask(), showAnswer:false}
+  }
+
+  net = () => getNet(this.state.ip, this.state.mask);
+
+  showAnswer = () => this.setState({showAnswer:true})
+
+  render() {
+    return (
+      <div>
+        <p>По заданным IP-адресу узла и маске определите адрес сети:</p>
+        <p>IP-адрес: {this.state.ip.join('.')} Маска: {this.state.mask.join('.')}</p>
+        <p onClick={this.showAnswer}>Ответ: <span className={this.state.showAnswer ? '' : 'hide'}>{this.net().join('.')}</span></p>
+      </div>
+    );
+  }
+
+}
+
+export default App
